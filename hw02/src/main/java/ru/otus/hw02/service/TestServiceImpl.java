e4a42066405ab1c3247f67c35addc59cf28f492d
Matvei <matvei@example.com>
2023-11-06 11:18:47 +0300

    hw02 done for spring-context.xml

--- hw02/src/main/java/ru/otus/hw02/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw02/service/TestServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw02.service;
 
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw02.dao.QuestionDao;
+import ru.otus.hw02.domain.Answer;
 import ru.otus.hw02.domain.Student;
 import ru.otus.hw02.domain.TestResult;
 
@@ -19,8 +20,21 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
+        for (var question : questions) {
             var isAnswerValid = false; // Задать вопрос, получить ответ
+            ioService.printLine(question.text());
+            int answerValidNum = 0;
+            int countAnswer = 0;
+            for (Answer answer : question.answers()) {
+                ioService.printLine(++countAnswer + ": " + answer.text());
+                if (answer.isCorrect()) {
+                    answerValidNum = countAnswer;
+                }
+            }
+            String yourAnswer = ioService.readStringWithPrompt("your answer");
+            if (Integer.parseInt(yourAnswer) == answerValidNum) {
+                isAnswerValid = true;
+            }
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
