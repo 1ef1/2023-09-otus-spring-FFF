43295f5348c4e34b5c6aaa675512908a93347d11
Matvei <matvei@example.com>
2023-11-06 16:59:07 +0300

    hw02 done контекст на основе Annotation/Java конфигурирования

--- hw02/src/main/java/ru/otus/hw02/Application.java
+++ hw02/src/main/java/ru/otus/hw02/Application.java
@@ -1,16 +1,17 @@
 package ru.otus.hw02;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.otus.hw02.config.AppConfig;
 import ru.otus.hw02.service.TestRunnerService;
 
+//@ComponentScan(basePackages = "ru.otus.hw02")
 public class Application {
     public static void main(String[] args) {
 
         //Создать контекст на основе Annotation/Java конфигурирования
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
         var testRunnerService = context.getBean(TestRunnerService.class);
         testRunnerService.run();
-
     }
 }
--- hw02/src/main/java/ru/otus/hw02/config/AppConfig.java
+++ hw02/src/main/java/ru/otus/hw02/config/AppConfig.java
@@ -2,25 +2,64 @@ package ru.otus.hw02.config;
 
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import ru.otus.hw02.dao.CsvQuestionDao;
+import ru.otus.hw02.service.ResultServiceImpl;
+import ru.otus.hw02.service.StreamsIOService;
+import ru.otus.hw02.service.StudentServiceImpl;
+import ru.otus.hw02.service.TestServiceImpl;
+import ru.otus.hw02.service.TestRunnerServiceImpl;
 
+@Configuration
+@ComponentScan(basePackages = "ru.otus.hw02")
+@PropertySource("classpath:application.properties")
 @Data
 public class AppConfig implements TestConfig, TestFileNameProvider {
-
-    // внедрить свойство из application.properties
+    @Value("${rightAnswersCountToPass}")
     private int rightAnswersCountToPass;
 
-    // внедрить свойство из application.properties
+    @Value("${testFileName}")
     private String testFileName;
 
-
-
-    @Override
-    public int getRightAnswersCountToPass() {
-        return rightAnswersCountToPass;
+    @Bean
+    public CsvQuestionDao csvQuestionDao() {
+        return new CsvQuestionDao(appConfig());
     }
 
-    @Override
-    public String getTestFileName() {
-        return testFileName;
+    @Bean
+    public StreamsIOService ioService() {
+        return new StreamsIOService(System.out, System.in);
+    }
+
+    @Bean
+    public TestServiceImpl testServiceImpl() {
+        return new TestServiceImpl(ioService(), csvQuestionDao());
+    }
+
+    @Bean
+    public StudentServiceImpl studentService() {
+        return new StudentServiceImpl(ioService());
+    }
+
+    @Bean
+    public ResultServiceImpl resultService() {
+        return new ResultServiceImpl(appConfig(), ioService());
+    }
+
+    @Bean
+    public TestRunnerServiceImpl testRunnerServiceImpl() {
+        return new TestRunnerServiceImpl(testServiceImpl(), studentService(), resultService());
+    }
+
+    @Bean(name = "myAppConfig")
+    public AppConfig appConfig() {
+        AppConfig config = new AppConfig();
+        config.setRightAnswersCountToPass(rightAnswersCountToPass);
+        config.setTestFileName(testFileName);
+        return config;
     }
 }
