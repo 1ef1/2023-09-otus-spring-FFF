ec9fc468032c5d08fcea32ff1b40248352744f30
Matvei <matvei@example.com>
2023-12-09 12:41:52 +0300

    hw04 SpringBootTest

--- hw03-spring-boot/src/test/java/ru/otus/hw03/dao/CsvQuestionDaoIntegrationTest.java
+++ hw03-spring-boot/src/test/java/ru/otus/hw03/dao/CsvQuestionDaoIntegrationTest.java
@@ -1,29 +1,31 @@
 package ru.otus.hw03.dao;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import ru.otus.hw03.config.TestFileNameProvider;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.otus.hw03.config.AppConfig;
 import ru.otus.hw03.domain.Question;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class CsvQuestionDaoIntegrationTest {
 
-    @Mock
-    private TestFileNameProvider testFileNameProvider;
+    @MockBean
+    private AppConfig appConfig;
 
     private static final int EXPECTED_QUESTION_COUNT = 6;
+
     @Test
     void shouldFindAllQuestions() {
-        Mockito.when(testFileNameProvider.getTestFileName()).thenReturn("questions.csv");
-        CsvQuestionDao csvQuestionDao = new CsvQuestionDao(testFileNameProvider);
+        when(appConfig.getTestFileName()).thenReturn("questions.csv");
+
+        CsvQuestionDao csvQuestionDao = new CsvQuestionDao(appConfig);
+
         List<Question> questions = csvQuestionDao.findAll();
         assertNotNull(questions);
         assertEquals(EXPECTED_QUESTION_COUNT, questions.size());
--- hw03-spring-boot/src/test/java/ru/otus/hw03/service/TestServiceImplTest.java
+++ hw03-spring-boot/src/test/java/ru/otus/hw03/service/TestServiceImplTest.java
@@ -2,9 +2,9 @@ package ru.otus.hw03.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.hw03.dao.CsvQuestionDao;
 import ru.otus.hw03.domain.Answer;
 import ru.otus.hw03.domain.Question;
@@ -16,18 +16,21 @@ import java.util.List;
 
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class TestServiceImplTest {
-    @Mock
+
+    @MockBean
     private CsvQuestionDao csvQuestionDao;
 
-    @Mock
+    @MockBean
     private LocalizedIOService ioService;
+
+    @Autowired
+    private TestServiceImpl testService;
 
     @Test
     void executeTestFor() {
-        when(ioService.readIntForRangeWithPromptLocalized(1, 3, "TestService.answer.the.answer",
-                "TestService.answer.the.error")).thenReturn(2);
+
         String textQuestion = "Question1";
         Answer answer1 = new Answer("Answer1", false);
         Answer answer2 = new Answer("Answer2", true);
@@ -39,8 +42,9 @@ public class TestServiceImplTest {
         Question question = new Question(textQuestion, answerList);
         List<Question> questionList = new ArrayList<>();
         questionList.add(question);
+        when(ioService.readIntForRangeWithPromptLocalized(1, 3, "TestService.answer.the.answer",
+                "TestService.answer.the.error")).thenReturn(2);
         when(csvQuestionDao.findAll()).thenReturn(questionList);
-        TestServiceImpl testService = new TestServiceImpl(ioService, csvQuestionDao);
         TestResult testResult = testService.executeTestFor(new Student("firstNameStudent", "lastNameStudent"));
         Assertions.assertEquals(1, testResult.getRightAnswersCount());
     }
