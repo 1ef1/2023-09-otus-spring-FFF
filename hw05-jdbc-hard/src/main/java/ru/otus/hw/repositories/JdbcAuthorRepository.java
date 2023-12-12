df11154dac49d5d8022a839f0c46a1f34d24b6f9
Matvei <matvei@example.com>
2023-12-12 20:39:48 +0300

    hw05 continue1

--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,33 +1,50 @@
 package ru.otus.hw.repositories;
 
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class JdbcAuthorRepository implements AuthorRepository {
+    private final JdbcOperations jdbc;
+
+    private final NamedParameterJdbcOperations namedParameterJdbcOperations;
+
+    public JdbcAuthorRepository(JdbcOperations jdbc, NamedParameterJdbcOperations namedParameterJdbcOperations) {
+        this.jdbc = jdbc;
+        this.namedParameterJdbcOperations = namedParameterJdbcOperations;
+    }
 
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+        return jdbc.query("select id, full_name from authors", new AuthorRowMapper());
     }
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+
+        String sql = "select id, full_name from authors where id = :id";
+        Map<String, Object> namedParameters = Collections.singletonMap("id", id);
+        List<Author> authors = namedParameterJdbcOperations.query(sql, namedParameters,new AuthorRowMapper());
+        return authors.stream().findFirst();
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
         public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            long id = rs.getLong("id");
+            String fullName = rs.getString("full_name");
+            return new Author(id, fullName);
         }
     }
 }
--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -1,33 +1,50 @@
 package ru.otus.hw.repositories;
 
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.Collections;
 
 @Repository
 public class JdbcGenreRepository implements GenreRepository {
+    private final JdbcOperations jdbc;
+
+    private final NamedParameterJdbcOperations namedParameterJdbcOperations;
+
+    public JdbcGenreRepository(JdbcOperations jdbc, NamedParameterJdbcOperations namedParameterJdbcOperations) {
+        this.jdbc = jdbc;
+        this.namedParameterJdbcOperations = namedParameterJdbcOperations;
+    }
 
     @Override
     public List<Genre> findAll() {
-        return new ArrayList<>();
+        return jdbc.query("select id, name from genres", new GnreRowMapper());
     }
 
     @Override
     public List<Genre> findAllByIds(Set<Long> ids) {
-        return new ArrayList<>();
+        String sql = "select id, name from genre where id in (:ids)";
+
+        Map<String, Object> namedParameters = Collections.singletonMap("ids", ids);
+
+        return namedParameterJdbcOperations.query(sql, namedParameters,new GnreRowMapper());
     }
 
     private static class GnreRowMapper implements RowMapper<Genre> {
 
         @Override
         public Genre mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            return new Genre(id, name);
         }
     }
 }
