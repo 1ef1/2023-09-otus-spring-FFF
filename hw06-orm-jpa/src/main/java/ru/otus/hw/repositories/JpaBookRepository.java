512ada845e9924b62877a615084d0c5b7563e130
Matvei <matvei@example.com>
2023-12-21 14:51:58 +0300

    hw06 continue2 add Jpa

---
+++ hw06-orm-jpa/src/main/java/ru/otus/hw/repositories/JpaBookRepository.java
@@ -0,0 +1,48 @@
+package ru.otus.hw.repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JpaBookRepository implements BookRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Optional<Book> findById(long id) {
+        return Optional.ofNullable(em.find(Book.class, id));
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return em.createQuery("SELECT b FROM Book b JOIN FETCH b.author JOIN FETCH b.genres"
+                , Book.class).getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(long id) {
+        Book book = em.find(Book.class, id);
+        if (book != null) {
+            em.remove(book);
+        }
+    }
+
+    // The update logic can be done by fetching and modifying the entity or using merge
+    @Transactional
+    public Book save(Book book) {
+        if (book.getId() == 0) {
+            em.persist(book);
+            return book;
+        } else {
+            return em.merge(book);
+        }
+    }
+}
---
+++ hw06-orm-jpa/src/main/java/ru/otus/hw/repositories/JpaGenreRepository.java
@@ -0,0 +1,34 @@
+package ru.otus.hw.repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Genre;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public class JpaGenreRepository implements GenreRepository {
+
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Genre> findAll() {
+        return entityManager.createQuery("SELECT g FROM Genre g", Genre.class).getResultList();
+    }
+
+    @Override
+    public List<Genre> findAllByIds(Set<Long> ids) {
+        if (ids == null || ids.isEmpty()) {
+            return Collections.emptyList();
+        }
+        TypedQuery<Genre> query = entityManager.createQuery("SELECT g FROM Genre g WHERE g.id IN :ids", Genre.class);
+        query.setParameter("ids", ids);
+        return query.getResultList();
+    }
+}
