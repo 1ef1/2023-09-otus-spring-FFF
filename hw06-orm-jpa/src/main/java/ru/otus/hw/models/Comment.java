70e0c4bd899d7b0b1341ccf3693fa316210b8b69
Matvei <matvei@example.com>
2023-12-29 12:32:50 +0300

    hw06 fix1.2

--- hw06-orm-jpa/src/main/java/ru/otus/hw/models/Comment.java
+++ hw06-orm-jpa/src/main/java/ru/otus/hw/models/Comment.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,7 +22,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "book_id", nullable = false)
+    @JoinColumn(name = "book_id", nullable = false)
     private long bookId;
 
     @Column(name = "comment_text", nullable = false)
