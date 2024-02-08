18262282bed5c7afe3d1d6b76dbc00c8a7f23e93
Matvei <matvei@example.com>
2024-02-08 20:07:25 +0300

    hw07 fix 2.1

--- hw07-spring-data/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw07-spring-data/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,18 +1,11 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    Optional<Comment> findById(Long id);
-
-    @Query("SELECT c FROM Comment c WHERE c.book.id = :bookId")
     List<Comment> findAllByBookId(Long bookId);
-
-    void deleteById(Long id);
 }
